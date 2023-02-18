3e93d2412d24ef3961b229e8ab380bf6c2438300
Kelvin Kijanda <67392007+K-Kelvin@users.noreply.github.com>
2023-02-18 17:48:26 +0000

    build: added all fonts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,95 +1,105 @@
 import styled from "styled-components";
 
-import linkedin from "../assets/linkedin.svg"
-import instagram from "../assets/instagram.svg"
-import twitter from "../assets/twitter.svg"
+import linkedin from "../assets/linkedin.svg";
+import instagram from "../assets/instagram.svg";
+import twitter from "../assets/twitter.svg";
 import EmailInput from "./EmailInput";
 
 export default function Footer() {
-    return (
-        <StyledFooter>
-            <div className="top">
-                <div className="links">
-                    <a href="#home">Home</a>
-                    <a href="#services">Services</a>
-                    <a href="#our-works">Our Works</a>
-                    <a href="#clients">Clients</a>
-                    <a href="#contact">Contact</a>
-                </div>
-                <div className="flex-mid" id="contact">
-                    <p className="edie">Edie</p>
-                    <div className="icons">
-                        <img src={linkedin} alt="alt" />
-                        <img src={instagram} alt="alt" />
-                        <img src={twitter} alt="alt" />
-                    </div>
-                </div>
-                <div className="contact-div">
-                    <p style={{color: "#828282", fontSize: 10}}>Want us to contact you?</p>
-                    <EmailInput bgColor="#F2F2F2" />
-                </div>
-            </div>
-            <p className="bottom">
-                created by <span>Kijanda</span> - devchallenges.io
-            </p>
-        </StyledFooter>
-    )
+	return (
+		<StyledFooter>
+			<div className="top">
+				<div className="links">
+					<a href="#home">Home</a>
+					<a href="#services">Services</a>
+					<a href="#our-works">Our Works</a>
+					<a href="#clients">Clients</a>
+					<a href="#contact">Contact</a>
+				</div>
+				<div className="flex-mid" id="contact">
+					<p className="edie">Edie</p>
+					<div className="icons">
+						<img src={linkedin} alt="alt" />
+						<img src={instagram} alt="alt" />
+						<img src={twitter} alt="alt" />
+					</div>
+				</div>
+				<div className="contact-div">
+					<p style={{ color: "#828282", fontSize: 10 }}>
+						Want us to contact you?
+					</p>
+					<EmailInput bgColor="#F2F2F2" />
+				</div>
+			</div>
+			<p className="bottom">
+				created by <span>Kijanda</span> - devchallenges.io
+			</p>
+		</StyledFooter>
+	);
 }
 
 const StyledFooter = styled.footer`
-    background-color: #100E1D;
-    padding: 40px 36px;
-    color: #F2F2F2;
+	background-color: #100e1d;
+	padding: 40px 36px;
+	color: #f2f2f2;
 
-    a {
-        color: #F2F2F2;
-    }
+	a {
+		color: #f2f2f2;
+	}
 
-    .top {
-        display: flex;
-        flex-direction: column;
-        gap: 4rem; /* 64x / 16px = 4rem */
-        margin-bottom: 5rem; /* 80px / 16px = 5rem*/
+	.top {
+		display: flex;
+		flex-direction: column;
+		gap: 4rem; /* 64x / 16px = 4rem */
+		margin-bottom: 5rem; /* 80px / 16px = 5rem*/
 
-        .links {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 1rem;
+		.links {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			gap: 1rem;
 
-            a {
-                display: inline-block;
-                text-decoration: none;
+			a {
+				display: inline-block;
+				text-decoration: none;
 
-                &:hover {
-                    text-decoration: underline;
-                }
-            }
-        }
+				&:hover {
+					text-decoration: underline;
+				}
+			}
+		}
 
-        .flex-mid {
-            .edie {
-                font-size: 2.25rem;
-                font-weight: 800;
-                color: inherit;
-                margin-bottom: 0.5rem;
-            }
-            .icons {
-                display: flex;
-                gap: 0.75rem; /* 0.75rem = 12px */
-            }
-        }
-    }
+		.flex-mid {
+			.edie {
+				margin-bottom: 0.5rem;
+				font-family: "Heebo", sans-serif;
+				font-style: normal;
+				font-weight: 800;
+				font-size: 2.25rem; /* 36px  */
+				line-height: 53px;
+				color: #f2f2f2;
+			}
+			.icons {
+				display: flex;
+				gap: 0.75rem; /* 0.75rem = 12px */
+			}
+		}
+	}
 
-    .bottom {
-        text-align: center;
-        color: white;
+	.bottom {
+		color: #f2f2f2;
+		font-family: "Montserrat", sans-serif;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 0.875rem; /* 14px */
+		line-height: 17px;
+		text-align: center;
 
-        span {
-            font-weight: bold;
-            font-style: underline;
-            text-decoration: underline;
-            cursor: pointer;
-        }
-    }
-`
+		span {
+			font-weight: bold;
+			font-style: underline;
+			text-decoration: underline;
+			cursor: pointer;
+		}
+	}
+`;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,93 +1,101 @@
 import styled from "styled-components";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { useRef } from "react";
 
-export default function Header () {
-    const menuRef = useRef();
+export default function Header() {
+	const menuRef = useRef();
 
-    const toggleMenu = () => {
-        menuRef.current.classList.toggle("hidden");
-    }
+	const toggleMenu = () => {
+		menuRef.current.classList.toggle("hidden");
+	};
 
-    return (
-        <StyledHeader>
-            <div className="container">
-                <a href="/" alt="Edie" className="logo">Edie</a>
-                <MenuIcon onClick={toggleMenu} className="menu-icon" />
-            </div>
-            <div className="menu hidden" ref={menuRef}>
-                <a href="#home">Home</a>
-                <a href="#services">Services</a>
-                <a href="#our-works">Our Works</a>
-                <a href="#clients">Clients</a>
-                <a href="#contact">Contact</a>
-            </div>
-        </StyledHeader>
-    )
+	return (
+		<StyledHeader>
+			<div className="container">
+				<a href="/" alt="Edie" className="logo">
+					Edie
+				</a>
+				<MenuIcon onClick={toggleMenu} className="menu-icon" />
+			</div>
+			<div className="menu hidden" ref={menuRef}>
+				<a href="#home">Home</a>
+				<a href="#services">Services</a>
+				<a href="#our-works">Our Works</a>
+				<a href="#clients">Clients</a>
+				<a href="#contact">Contact</a>
+			</div>
+		</StyledHeader>
+	);
 }
 
 const StyledHeader = styled.header`
-    background-color: #2D9CDB;
-    color: #F2F2F2;
-    padding: 12px 10px;
-    margin-bottom: 36px;
+	background-color: #2d9cdb;
+	color: #f2f2f2;
+	padding: 12px 10px;
+	margin-bottom: 36px;
 
-    .menu-icon {
-        cursor: pointer;
-    }
+	.menu-icon {
+		cursor: pointer;
+	}
 
-    @media only screen and (min-width: 768px) { /* on tablet and desktops */
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+	@media only screen and (min-width: 768px) {
+		/* on tablet and desktops */
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 
-        .menu-icon {
-            display: none;
-        }
-    }
+		.menu-icon {
+			display: none;
+		}
+	}
 
-    .container {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
 
-    & .logo {
-        display: block;
-        font-size: 24px;
-        font-weight: 800;
-        text-decoration: none; 
-        color: inherit;
-    }
+	& .logo {
+		display: block;
+		text-decoration: none;
+		font-family: "Heebo", sans-serif;
+		font-style: normal;
+		font-weight: 800;
+		font-size: 1.5rem; /* 24px */
+		line-height: 35px;
+		color: #333333;
+	}
 
-    .menu {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 1rem;
-        margin-top: 1rem;
-        
-        @media only screen and (min-width: 768px) { /* on tablet and desktops */
-            margin-top: 0;
-            flex-direction: row;
-            align-items: center;
-            gap: 2rem;
-        }
-    
-        a {
-            color: #E1D6D6;
-            text-decoration: none;
+	.menu {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 1rem;
+		margin-top: 1rem;
 
-            &:hover {
-                text-decoration: underline;
-            }
-        }
-    }
+		@media only screen and (min-width: 768px) {
+			/* on tablet and desktops */
+			margin-top: 0;
+			flex-direction: row;
+			align-items: center;
+			gap: 2rem;
+		}
 
-    .hidden {
-        display: none;
-        @media only screen and (min-width: 768px) { /* on tablet and desktops */
-            display: flex;
-        }
-    }
-`
+		a {
+			color: #e1d6d6;
+			text-decoration: none;
+
+			&:hover {
+				text-decoration: underline;
+			}
+		}
+	}
+
+	.hidden {
+		display: none;
+		@media only screen and (min-width: 768px) {
+			/* on tablet and desktops */
+			display: flex;
+		}
+	}
+`;
